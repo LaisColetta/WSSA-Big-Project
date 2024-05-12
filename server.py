dfafaf5e70e4e646a493b1d9c5bcc67f0d3ff260
LaisColetta <laiscoletta@gmail.com>
2024-05-12 20:49:45 +0100

    connetion with onlinedatabase

--- server.py
+++ server.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from config import Config as cfg
 
 app = Flask(__name__)
+app.config.from_object(cfg)  # Load configuration from config.py
+db = SQLAlchemy(app)
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Recipe {self.id} - {self.title}>"
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"<Category {self.id} - {self.name}>"
 
 @app.route('/')
 def index():
-    return 'Hello lala!'
+    recipes = Recipe.query.all()
+    return render_template('index.html', recipes=recipes)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
