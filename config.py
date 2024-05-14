cba49de614d4baad0b89d0c947e0418973c9aef1
LaisColetta <99751201+LaisColetta@users.noreply.github.com>
2024-05-14 15:46:33 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
 class config:
     DEBUG = True
     SECRET_KEY = 'recipe123'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'root'
-    MYSQL_DATABASE = 'recipedb'
-
+    SQLITE_DATABASE_PATH = 'C:\\Users\\appDB\\Desktop\\WSSA-Big-Project\\recipes'
