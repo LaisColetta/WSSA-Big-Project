5c76c6a832385a99a7abf62ef81fa3547f8fcfdd
LaisColetta <laiscoletta@gmail.com>
2024-05-14 21:22:55 +0100

    new pythonanywhere account

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class config:
     DEBUG = True
-    MYSQL_DATABASE_HOST = 'laiscoletta.mysql.pythonanywhere-services.com'
-    MYSQL_DATABASE_USER = 'laiscoletta'
+    MYSQL_DATABASE_HOST = 'laispythonanywhere.mysql.pythonanywhere-services.com'
+    MYSQL_DATABASE_USER = 'laispythonanywhe'
     MYSQL_DATABASE_PASSWORD = 'recipe123'
-    MYSQL_DATABASE_DB = 'laiscoletta$recipedb'
+    MYSQL_DATABASE_DB = 'laispythonanywhe$recipedb'
