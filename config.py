63d9fc68bbbacb776852d3d5e82f0a24b6f30ed4
laiscoletta <you@example.com>
2024-05-14 19:26:51 +0000

    pa

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 class config:
     DEBUG = True
-    SECRET_KEY = 'recipe123'
     MYSQL_DATABASE_HOST = 'laiscoletta.mysql.pythonanywhere-services.com'
     MYSQL_DATABASE_USER = 'laiscoletta'
     MYSQL_DATABASE_PASSWORD = 'recipe123'
-    MYSQL_DATABASE_DB = 'recipedb'
+    MYSQL_DATABASE_DB = 'laiscoletta$recipedb'
