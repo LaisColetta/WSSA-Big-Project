ae0a00dfc0ece3fe626a84805bc00b627fefb088
LaisColetta <laiscoletta@gmail.com>
2024-05-15 21:02:46 +0100

    configPA

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class config:
     DEBUG = True
-    MYSQL_DATABASE_HOST = 'laispythonanywhere.mysql.pythonanywhere-services.com'
-    MYSQL_DATABASE_USER = 'laispythonanywhe'
+    MYSQL_DATABASE_HOST = 'lais.mysql.pythonanywhere-services.com'
+    MYSQL_DATABASE_USER = 'lais'
     MYSQL_DATABASE_PASSWORD = 'recipe123'
-    MYSQL_DATABASE_DB = 'laispythonanywhe$recipedb'
+    MYSQL_DATABASE_DB = 'lais$recipedb'
